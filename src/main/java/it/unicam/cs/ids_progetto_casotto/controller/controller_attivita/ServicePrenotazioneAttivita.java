c6d2e54e6346640515496b51b02ef8a69ba684d7
Lorenzo Spina <lorenzospi00@hotmail.it>
2022-02-22 11:45:45 +0100

    Complemtamento Attività Spring

--- src/main/java/it/unicam/cs/ids_progetto_casotto/controller/controller_attivita/ServicePrenotazioneAttivita.java
+++ src/main/java/it/unicam/cs/ids_progetto_casotto/controller/controller_attivita/ServicePrenotazioneAttivita.java
@@ -1,5 +1,6 @@
 package it.unicam.cs.ids_progetto_casotto.controller.controller_attivita;
 
+import it.unicam.cs.ids_progetto_casotto.model.attivita.Attivita;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -7,31 +8,36 @@ import java.util.Optional;
 @Service
 public class ServicePrenotazioneAttivita {
 
-    public ServicePrenotazioneAttivita(){
+    private RepositoryPrenotazioneAttivita repositoryPrenotazioneAttivita;
+    private RepositoryAttivita repositoryAttivita;
+
+    public ServicePrenotazioneAttivita(RepositoryPrenotazioneAttivita repositoryPrenotazioneAttivita, RepositoryAttivita repositoryAttivita){
+        this.repositoryPrenotazioneAttivita= repositoryPrenotazioneAttivita;
+        this.repositoryAttivita= repositoryAttivita;
 
     }
 
-    public Optional<PrenotazioneAttivitaCliente> creaPrenotazioneAttivita(int idCliente, Integer id) {
+    public Optional<PrenotazioneAttivitaCliente> creaPrenotazioneAttivita(Integer idCliente, Integer id) {
 
-        /*Optional<Attivita> attivitaSelezionata = this.repositoryAttivita.findById(id);
+        Optional<Attivita> attivitaSelezionata = this.repositoryAttivita.findById(id);
         PrenotazioneAttivitaCliente nuovaPrenotazione = new PrenotazioneAttivitaCliente();
         nuovaPrenotazione.setAttivitaPrenotata(attivitaSelezionata.get());
         nuovaPrenotazione.setIdCliente(idCliente);
         nuovaPrenotazione.setOrarioPrenotazione();
-        nuovaPrenotazione.setIdCliente(id);
+        nuovaPrenotazione.setIdPrenotazione(id);
 
 
-        return Optional.of(this.repositoryPrenotazioneAttivita.save(nuovaPrenotazione));*/
-        return Optional.empty();
+        return Optional.of(this.repositoryPrenotazioneAttivita.save(nuovaPrenotazione));
+
     }
 
     public Optional<PrenotazioneAttivitaCliente> eliminaPrenotazioneAttivitaCliente(Integer idPrenotazione) {
-        //<PrenotazioneAttivitaCliente> toDelete = this.repositoryPrenotazioneAttivita.findById(idPrenotazione);
-        //if(toDelete.isEmpty()){
-        //    return Optional.empty();
-        //}
-        //this.repositoryPrenotazioneAttivita.deleteById(idPrenotazione);
-        return Optional.empty();
+     Optional <PrenotazioneAttivitaCliente> toDelete = this.repositoryPrenotazioneAttivita.findById(idPrenotazione);
+        if(toDelete.isEmpty()){
+            return Optional.empty();
+        }
+        this.repositoryPrenotazioneAttivita.deleteById(idPrenotazione);
+       return toDelete;
 
     }
 }
